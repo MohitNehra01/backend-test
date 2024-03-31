3369328058ac3dcaa8162db231dce51ee3bbacf0
Mohit Nehra <mohitnehra895@gmail.com>
2024-03-31 20:39:20 +0530

    some changes in multer middleware

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -6,7 +6,6 @@ const upload = multer({
     dest: "uploads/",
     limits: {fileSize: 50 * 1024*1024} , // 50 mb in size max limit
     storage: multer.diskStorage({
-        destination: "uploads/",
         filename: (_req,file,cb) =>{
             cb(null , file.originalname);
         },
